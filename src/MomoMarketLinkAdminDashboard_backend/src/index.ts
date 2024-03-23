2816b07185803e52b8c2110f67000c4d12459432
ngalande <blessngalande@gmail.com>
2024-03-23 22:20:32 +0200

    update

--- src/MomoMarketLinkAdminDashboard_backend/src/index.ts
+++ src/MomoMarketLinkAdminDashboard_backend/src/index.ts
@@ -3,5 +3,11 @@ import { Canister, query, text } from 'azle';
 export default Canister({
     greet: query([text], text, (name) => {
         return `Hello, ${name}!`;
+    }),
+
+    getBuyers: query([text], text, (name) => {
+        return `Buyers endpoint, ${name}!`;
     })
 })
+
+
